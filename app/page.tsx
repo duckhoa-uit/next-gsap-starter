c1f9aaf24998b40d1e8484cb57bf40f819e478b0
DeeNKay <duckhoa.dev@gmail.com>
2025-02-15 09:30:58 +0700

    chore: upgrade tailwind v4

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 						<div className="overflow-hidden">
 							<li className="mb-2">
 								Get started by editing{" "}
-								<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+								<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
 									app/page.tsx
 								</code>
 								.
